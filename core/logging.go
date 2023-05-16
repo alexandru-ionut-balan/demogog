54e2d46f2e85af2edd633d6104458dd34ce5cb66
Alexandru - Ionut Balan <alexandru-ionut.balan@ing.com>
2023-05-16 15:21:52 +0300

    [NEW] Complete digest method supporting SHA-256 and SHA-512 algorithms

--- core/logging.go
+++ core/logging.go
@@ -1,6 +1,33 @@
 package core
 
-import "go.uber.org/zap"
+import (
+	"fmt"
+
+	"go.uber.org/zap"
+)
 
 var logger, _ = zap.NewProduction()
-var Log = logger.Sugar()
+
+var Log = OpenBankingLogger{}
+
+type OpenBankingLogger struct {
+	*zap.SugaredLogger
+}
+
+func (obl *OpenBankingLogger) Info(args ...any) {
+	obl.Infoln(args)
+}
+
+func (obl *OpenBankingLogger) Warn(args ...any) {
+	obl.Warnln(args)
+}
+
+func (obl *OpenBankingLogger) Error(args ...any) {
+	obl.Errorln(args)
+}
+
+func (obl *OpenBankingLogger) Sync() {
+	if err := obl.SugaredLogger.Sync(); err != nil {
+		fmt.Println(err.Error())
+	}
+}
--- signature/digest/digest.go
+++ signature/digest/digest.go
@@ -3,6 +3,7 @@ package digest
 import (
 	"demogog/core"
 	"net/http"
+	"strings"
 )
 
 // Digest will compute the digest of your payload and add a new entry to
@@ -10,5 +11,14 @@ import (
 //
 // The digest header is: "Digest": "{hashingAlgorithmName}=base64(hashingAlgorithm(payload))"
 func Digest(hashingAlgorithm core.HashingAlgorithm, payload string, header *http.Header) {
+	digest := strings.ToUpper(string(hashingAlgorithm)) + "="
 
+	switch hashingAlgorithm {
+	case core.SHA256:
+		digest += core.Base64(core.Sha256([]byte(payload)))
+	default:
+		digest += core.Base64(core.Sha512([]byte(payload)))
+	}
+
+	header.Add("Digest", digest)
 }
